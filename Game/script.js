0e1306b1dae3e4e4673b165adf37f69f4012bbc0
klikkerms <62421458+klikkerms@users.noreply.github.com>
2022-11-15 21:15:16 +0300

    Крестики нолики

---
+++ Game/script.js
@@ -0,0 +1,57 @@
+const requestUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+
+
+function radik()
+{
+    var radik_check;
+    radik_check = document.getElementsByName('r1');
+    for(var i = 0; i < radik_check.length; i++)
+    {
+        if(radik_check[i].checked)
+        {
+            //alert('Выбран элемент ' + i)
+            const body = {
+                id_game: i
+            }
+            //alert(JSON.stringify(body))
+
+
+
+            return new Promise((resolve,reject)=>{
+                const xhr = new XMLHttpRequest()
+
+                xhr.open('POST', requestUrl)
+
+                xhr.responseType = 'json'
+                xhr.setRequestHeader('Content-type','application/json')
+
+                xhr.onload = () => {
+                    if (xhr.status >= 400) {
+                        reject(xhr.response)
+                    }
+                    else{
+                        resolve(xhr.response)
+                    }
+                }
+                xhr.onerror = () => {
+                    reject(xhr.response)
+                }
+
+                xhr.send(JSON.stringify(body))
+                xhr.onload = () => console.log(xhr.response);
+            })
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
